8c2806196bdd4774662427c748bb1273847fba7e
Harshita Kasera <hkasera5@gmail.com>
2016-12-04 18:42:32 -0800

    Fixed UI bug

--- core/es.js
+++ core/es.js
@@ -2,8 +2,8 @@ var elasticsearch = require('elasticsearch');
 var client = new elasticsearch.Client({
   host: [
     {
-      host: "6ababf46412e5cd9ff51a87e3b704981.us-west-1.aws.found.io",
-      auth: 'elastic:QHtaXf3aNCwJtuz66nKZfnRq',
+      host: process.env.ELASTIC_HOST,
+      auth: process.env.ELASTIC_USERNAME+':'+process.env.ELASTIC_PASS,
       protocol: 'http',
       port: 9200
     }
